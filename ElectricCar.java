469aa95e432d89839a2daca68112182925f8bd40
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2022-10-07 14:27:26 -0400

    Add files via upload

---
+++ ElectricCar.java
@@ -0,0 +1,65 @@
+package edu.dccc.TopElectricCars;
+import java.text.NumberFormat;
+
+class ElectricCar {
+    String model;
+    String imgSrc;
+    double basePrice;
+    int epaMaxRange;
+    String epaFuelEconomy;
+    int rank;
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public String getFormattedBasePrice() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return formatter.format(basePrice);
+    }
+
+    public int getEpaMaxRange() {
+        return epaMaxRange;
+    }
+
+    public String getEpaFuelEconomy() {
+        return epaFuelEconomy;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public ElectricCar(String model, String imgSrc, double basePrice, int epaMaxRange, String epaFuelEconomy, int rank) {
+        this.model = model;
+        this.imgSrc = imgSrc;
+        this.basePrice = basePrice;
+        this.epaMaxRange = epaMaxRange;
+        this.epaFuelEconomy = epaFuelEconomy;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "model='" + model + '\'' +
+                ", imgSrc='" + imgSrc + '\'' +
+                ", basePrice=" + basePrice +
+                ", epaMaxRange=" + epaMaxRange +
+                ", epaFuelEconomy='" + epaFuelEconomy + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
+
+}
+
+
+
---
+++ TopElectricCarList.jsp
@@ -0,0 +1,32 @@
+<%@ page import="edu.dccc.TopElectricCars.ElectricCar" %>
+<%@ page import="edu.dccc.TopElectricCars.ElectricCarList" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car and Driver Top Electric Cars</title>
+</head>
+<body>
+</h1>
+<br/>
+
+<h1>12 Top Selling Electric Vehicles of 2021</h1>
+<%
+    // Following 3 lines simulate retrieving data from a database or other data source
+    ElectricCarList cl = new ElectricCarList();
+    cl.populateElectricCarList();
+    ArrayList<ElectricCar> carList = cl.getCarList();
+
+    // Dynamically generate top car list
+    for (ElectricCar car: carList ) { %>
+<h2><%= car.getRank()%>. <%= car.getModel()%></h2>
+<img src=<%=car.getImgSrc()%>>
+<ul>
+    <li>Base price: <%=car.getFormattedBasePrice()%></li>
+    <li>EPA Fuel Economy combined/city/highway: <%=car.getEpaFuelEconomy()%></li>
+    <li>EPA Range: <%=car.getEpaMaxRange()%></li>
+</ul>
+<% } %>
+</body>
+</html>
